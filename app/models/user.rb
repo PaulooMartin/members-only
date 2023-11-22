f5010383ff024bd67bcfee1673a0a63d64009e73
PaulooMartin <paulmartinblopez@gmail.com>
2023-11-22 10:41:57 +0800

    Add username column to User (see more below)
    * Migrated the column
    * Added validations to username
    * Added username to valid parameters
    * Added username field to /sign-up and username text to /edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-  has_many :posts
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+  :rememberable, :validatable
+
+  validates :username, uniqueness: {case_sensitive: false }, presence: true,
+                      length: { minimum: 3 }, format: { without: /\W/,
+                      message: "can only have letters, numbers, and underlines" }
+  has_many :posts
+
 end
